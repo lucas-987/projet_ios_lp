a15b9fac38baba07fb3ca3f6be1c8bda44128bed
lucas-987 <lptb68720@gmai.com>
2021-01-04 12:34:11 +0100

    début partie 1

---
+++ to_do_TREYER_VANDAELE/TodoTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  TodoTableViewCell.swift
+//  to_do_TREYER_VANDAELE
+//
+//  Created by treyer lucas on 04/01/2021.
+//  Copyright © 2021 treyer lucas. All rights reserved.
+//
+
+import UIKit
+
+class TodoTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var todoTitleLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
